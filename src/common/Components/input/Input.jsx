3504b199abe1d11e61f9f0fcabb03c5524fd50c6
mac <mac@macs-MacBook-Air.local>
2025-03-31 02:16:26 +0530

    done

--- src/common/Components/input/Input.jsx
+++ src/common/Components/input/Input.jsx
@@ -12,14 +12,14 @@ const InputField = ({
 }) => {
   return (
     <div className={`w-full ${className}`}>
-      {label && <label className="block text-sm font-medium text-gray-700">{label}</label>}
+      {label && <label className="block text-sm font-medium text-gray-400">{label}</label>}
       <input
         type={type}
         name={name}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="mt-1 p-2 w-full  shadow border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        className="mt-1 p-2 w-full   border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
       />
       {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
     </div>
--- src/pages/auth/authantication/common/SectionHeader.jsx
+++ src/pages/auth/authantication/common/SectionHeader.jsx
@@ -2,21 +2,23 @@ import React from "react";
 
 const SectionHeader = ({ title, subtitle, description }) => {
     return (
-      <div className="text-center">
+      <div className="">
         <h1 className="text-5xl font-extrabold text-[var(--color-primary)] drop-shadow-md">
           {title}
         </h1>
-        <div className="w-24 h-1 bg-[var(--color-primary)] mx-auto mt-2 rounded-full"></div>
-        {subtitle && (
-          <p className="text-lg font-medium text-[var(--color--primary-text)] mt-3">
+        {/* <div className="w-24 h-1 bg-[var(--color-primary)] mx-auto mt-2 rounded-full"></div> */}
+      <div className="flex w-full justify-between items-center  h-14">
+      {subtitle && (
+          <p className="text-lg font-medium text-[var(--color--primary-text)] ">
             {subtitle}
           </p>
         )}
         {description && (
-          <p className="text-base text-[var(--color--primary-text)] mb-6 opacity-80">
+          <p className="text-base text-[var(--color--primary-text)]  opacity-80">
             {description}
           </p>
         )}
+      </div>
       </div>
     );
   };

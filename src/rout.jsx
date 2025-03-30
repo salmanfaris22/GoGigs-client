60b9f963ed021c30d157c50b5db97dfb90f172c8
mac <mac@macs-MacBook-Air.local>
2025-03-30 22:35:08 +0530

    done

--- src/rout.jsx
+++ src/rout.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 
 import AuthLayout from "./pages/auth/authantication/Auth";
 import AuthLogin from "./pages/auth/authantication/login/AuthLogin";
-import Authregister from "./pages/auth/authantication/register/AuthRegister";
+// import Authregister from "./pages/auth/authantication/register/AuthRegister";
 import VerifyOTP from "./pages/auth/authantication/verifyOTP/verifyOTP";
 import AuthForgotPassword from "./pages/auth/authantication/forgot/ForgotPassword";
 import NewPassword from "./pages/auth/authantication/newPassword/NewPassword";
@@ -19,7 +19,7 @@ const UserRout = () => {
 {/* <Route path="/" element={<Hero/> }/> */}
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
-          <Route path="register" element={<Authregister />} />
+          {/* <Route path="register" element={<Authregister />} /> */}
           <Route path="verify-otp/:meth/:email" element={<VerifyOTP />} />
           <Route path="forgot/:meth" element={<AuthForgotPassword />} />
           <Route path="new-password" element={<NewPassword />} />

283647032e036295d85e50f34a9973d2d5f54927
mac <mac@macs-MacBook-Air.local>
2025-04-05 15:00:20 +0530

    adf

--- src/pages/nonAuth/Hero/HeroPage.jsx
+++ src/pages/nonAuth/Hero/HeroPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { WobbleCards } from '../../../common/Components/WebbolCard/WebbleCard'
-import HeroGradiyentBtn from '../../../common/Components/button/HeroGradiyentBtn'
-import HeroMainText from '../../../common/UI/Hero text/HeroMainText'
-import HeroBottontext from '../../../common/UI/Hero text/HeroBottontext'
+// import { WobbleCards } from '../../../common/Components/WebbolCard/WebbleCard'
+// import HeroGradiyentBtn from '../../../common/Components/button/HeroGradiyentBtn'
+// import HeroMainText from '../../../common/UI/Hero text/HeroMainText'
+// import HeroBottontext from '../../../common/UI/Hero text/HeroBottontext'
 // import { NavbarDemo } from '../../../common/Layout/Navbar/Navbar'
 
 const Hero = () => {
   return (
     <div className=' w-[90%] m-auto'>
-    <div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] utils.ts before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-1 before:transform before:-translate-x-1/2">
+    {/* <div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] utils.ts before:bg-no-repeat before:bg-top before:bg-cover before:size-full before:-z-1 before:transform before:-translate-x-1/2">
       <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
     
        <HeroMainText/>
@@ -24,7 +24,7 @@ const Hero = () => {
         </div>
       </div>
       <WobbleCards/>
-    </div>
+    </div> */}
    
     </div>
   )
